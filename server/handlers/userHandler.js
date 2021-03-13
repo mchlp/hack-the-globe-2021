18e1846859ffd6c2ebfe2e5b81964c6e112818bc
Michael Pu <michael.pu123@gmail.com>
2021-03-13 13:55:36 -0500

    Add saving to JSON file, endpoint to add user

--- server/handlers/userHandler.js
+++ server/handlers/userHandler.js
@@ -1,7 +1,69 @@
+const fs = require('fs');
+
+const USER_DB_FILE_NAME = './handlers/userDb.json';
+
 const sampleUser = {
-    name: "Test Name",
-    company: "Test Company",
+    username: 'name',
+    name: 'Test Name',
+    company: 'Test Company',
     interests: [],
     history: [],
-    affiliations: []
+    affiliations: [],
+};
+
+class UserHandler {
+    constructor() {
+        this.userDb = JSON.parse(fs.readFileSync(USER_DB_FILE_NAME));
+        console.log(this.userDb);
+        setInterval(() => {
+            this.saveToFile();
+        }, 5000);
+    }
+
+    addUser(username, company) {
+        if (!this.userDb[username]) {
+            this.userDb[username] = {
+                username,
+                name: null,
+                company,
+                interests: [],
+                history: [],
+                affiliations: [],
+            };
+            return {
+                success: true,
+                data: this.userDb[username],
+            };
+        } else {
+            return {
+                success: false,
+                error: `user with username ${username} already exists.`,
+            };
+        }
+    }
+
+    updateUser(username, updatedUserData) {
+        if (this.userDb[username]) {
+            this.userDb[username] = {
+                ...this.userDb[username],
+                ...updatedUserData,
+            };
+            return {
+                success: true,
+                data: this.userDb[username],
+            };
+        } else {
+            return {
+                success: false,
+                error: `user with username ${username} does not exist`,
+            };
+        }
+    }
+
+    saveToFile() {
+        console.log("Saving user data file")
+        fs.writeFileSync(USER_DB_FILE_NAME, JSON.stringify(this.userDb, null, 4));
+    }
 }
+
+module.exports = UserHandler;
--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = 3001;
-const CharityHandler = require('./handlers/charityHandler')
+const CharityHandler = require('./handlers/charityHandler');
+const UserHandler = require('./handlers/userHandler');
 
 app.use(cors());
+app.use(express.json());
 
-const charityHandler = new CharityHandler()
+const charityHandler = new CharityHandler();
+const userHandler = new UserHandler();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -18,8 +21,16 @@ app.get('/test-endpoint', (req, res) => {
     });
 });
 
+app.post('/user/new', (req, res) => {
+    if (req.body && req.body.username && req.body.company) {
+        res.send(userHandler.addUser(req.body.username, req.body.company));
+    } else {
+        res.status(400).send("Body must contain username and company field.")
+    }
+});
+
 app.get('/charity/recommendation', (req, res) => {
-  res.send(charityHandler.getRecommendation());
+    res.send(charityHandler.getRecommendation());
 });
 
 app.listen(port, () => {
