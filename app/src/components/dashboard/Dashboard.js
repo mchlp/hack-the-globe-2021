1b18bb7fc07cc18fda11e74ec2ff3ad656ea9dae
Michael Pu <michael.pu123@gmail.com>
2021-03-14 03:02:25 -0400

    show better chips

--- app/src/components/dashboard/Dashboard.js
+++ app/src/components/dashboard/Dashboard.js
@@ -5,58 +5,74 @@ import graphImage from './graph.jpg';
 import axios from 'axios';
 import ButtonAppBar from './Navbar';
 
-const useStyles = makeStyles((theme) => ({
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  header: {
-    textAlign: "left",
-    fontFamily: "'Epilogue', sans-serif",
-    fontSize: "36px",
-    letterSpacing: "-1px",
-    color: '#338280',
-    fontWeight: "800",
-  },
-  titlebar: {
-    width: "100%",
-    backgroundColor: "white",
-  },
-  graphImage: {
-    width: "100%",
-    borderRadius: 5,
-  },
-  container: {
-    marginLeft: "250px",
-    marginRight: "250px",
-    paddingBottom: "100px",
-  },
-  header5: {
-    fontSize: "18px",
-  },
-  chip: {
-    backgroundColor: '#ECF1E0',
-    color: '#338280',
-    fontSize: "14px",
-  },
-  cardcontainer:{
-    padding: "16px 16px 0px",
-  },
-  description: {
-    display: "-webkit-box",
-    webkitLineClamp: "4",
-    webkitBoxOrient: "vertical",
-    overflow: "hidden",
+const categoryMappings = {
+    housing_and_homelessness: 'Homelessness',
+    youth: 'Youth',
+    environment: 'Environment',
+    other: 'Other',
+    international_aid: 'Crisis Response',
+    social_services: 'Social Services',
+    fundraising_organization: 'Fundraising',
+    education: 'Education',
+    gender_equality: 'Gender Equality',
+    health: 'Health',
+    local: 'Local',
+    lgbtqplus: 'LGBTQ+',
+    systemic_racism: 'Systemic Racism',
+};
 
-  },
-  button: {
-    backgroundColor: '#338280',
-    "&:hover": {
-      backgroundColor: "#19514F",
-  }}
+const useStyles = makeStyles((theme) => ({
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+    header: {
+        textAlign: 'left',
+        fontFamily: "'Epilogue', sans-serif",
+        fontSize: '36px',
+        letterSpacing: '-1px',
+        color: '#338280',
+        fontWeight: '800',
+    },
+    titlebar: {
+        width: '100%',
+        backgroundColor: 'white',
+    },
+    graphImage: {
+        width: '100%',
+        borderRadius: 5,
+    },
+    container: {
+        marginLeft: '250px',
+        marginRight: '250px',
+        paddingBottom: '100px',
+    },
+    header5: {
+        fontSize: '18px',
+    },
+    chip: {
+        backgroundColor: '#ECF1E0',
+        color: '#338280',
+        fontSize: '14px',
+    },
+    cardcontainer: {
+        padding: '16px 16px 0px',
+    },
+    description: {
+        display: '-webkit-box',
+        webkitLineClamp: '4',
+        webkitBoxOrient: 'vertical',
+        overflow: 'hidden',
+    },
+    button: {
+        backgroundColor: '#338280',
+        '&:hover': {
+            backgroundColor: '#19514F',
+        },
+    },
 }));
 
 const Dashboard = () => {
@@ -119,85 +135,184 @@ const Dashboard = () => {
         );
     } else {
         return (
-            <div>
+            <div style={{ paddingBottom: 20 }}>
                 <ButtonAppBar />
                 <div className={classes.container}>
                     <div>
                         <h1>Welcome {username}, here are your donations</h1>
                         <img src={graphImage} className={classes.graphImage} />
                     </div>
-                  </Card>
-                );
-              })}
-            </div>
-          </div>
-          <div>
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              <h1 className={classes.header}>Recommendations</h1>
-              <div style={{ marginLeft: 15 }}>
-                <Button className={classes.button}
-                  variant="contained"
-                  color="primary"
-                  onClick={updateRecommendations}
-                >
-                  Refresh
-                </Button>
-              </div>
-            </div>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-              }}
-            >
-              {recommendationList.map((recommendation) => {
-                return (
-                  <Card
-                    style={{ width: 250, height: 300, marginRight: 10 }}
-                    key={recommendation.id}
-                  >
-                    <div
-                      style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignContent: "space-between",
-                        alignItems: "space-between",
-                        justifyContent: "space-between",
-                        justifyItems: "space-between",
-                        height: "100%",
-                      }}
-                    >
-                      <div className={classes.cardcontainer}>
-                        <h3>{recommendation.name}</h3>
-                        <div>
-                          {recommendation.categories.map((category) => {
-                            return (
-                              <Chip className={classes.chip}
-                                color="primary"
-                                size="small"
-                                label={category}
-                              />
-                            );
-                          })}
-                        </div>
-                        <div style={{ fontSize: 14 }} className={classes.description}>
-                          {recommendation.description.substr(0, 200) + "..."}
-                        </div>
-                      </div>
-                      <Button className={classes.button}
-                        variant="contained"
-                        color="primary"
-                        disableElevation
-                        style={{ width: "100%" }}
-                      >
-                        Add to fund
-                      </Button>
+                </div>
+                <div>
+                    <div>
+                        <h1 className={classes.header}>Monthly Fund</h1>
+                        <h5 className={classes.header5}>
+                            Your fund includes the charities you’ve pledged to donate to monthly.
+                        </h5>
                     </div>
+                    <div
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                        }}
+                    >
+                        {monthlyFundList.map((recommendation) => {
+                            return (
+                                <Card style={{ width: 250, height: 330, marginRight: 10 }} key={recommendation.id}>
+                                    <div
+                                        style={{
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            alignContent: 'space-between',
+                                            alignItems: 'space-between',
+                                            justifyContent: 'space-between',
+                                            justifyItems: 'space-between',
+                                            height: '100%',
+                                        }}
+                                    >
+                                        <div className={classes.cardcontainer}>
+                                            <h3>{recommendation.name}</h3>
+                                            <div>
+                                                {recommendation.categories.map((category) => {
+                                                    return (
+                                                        <Chip
+                                                            className={classes.chip}
+                                                            color="primary"
+                                                            size="small"
+                                                            label={categoryMappings[category]}
+                                                        />
+                                                    );
+                                                })}
+                                            </div>
+                                            <div style={{ fontSize: 14 }} className={classes.description}>
+                                                {recommendation.description.substr(0, 200) + '...'}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Card>
+                            );
+                        })}
+                        <div
+                            style={{
+                                flexGrow: 1,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                justifyContent: 'space-between',
+                            }}
+                        >
+                            <Card
+                                style={{
+                                    marginBottom: 10,
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'row',
+                                        justifyContent: 'space-between',
+                                    }}
+                                >
+                                    <div>Budget</div>
+                                    <Link onClick={(e) => e.preventDefault()}>Change</Link>
+                                </div>
+                                <div>
+                                    <h3>$20/month</h3>
+                                </div>
+                            </Card>
+                            <Card
+                                style={{
+                                    marginBottom: 10,
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'row',
+                                        justifyContent: 'space-between',
+                                    }}
+                                >
+                                    <div>Next Donation</div>
+                                    <Link onClick={(e) => e.preventDefault()}>Change</Link>
+                                </div>
+                                <div>
+                                    <h3>04/14/2021</h3>
+                                </div>
+                            </Card>
+                            <Button variant="contained" color="primary" style={{ width: '100%' }}>
+                                Make a one-time donation
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                    }}
+                >
+                    <h1 className={classes.header}>Recommendations</h1>
+                    <div style={{ marginLeft: 15 }}>
+                        <Button
+                            className={classes.button}
+                            variant="contained"
+                            color="primary"
+                            onClick={updateRecommendations}
+                        >
+                            Refresh
+                        </Button>
+                    </div>
+                </div>
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                    }}
+                >
+                    {recommendationList.map((recommendation) => {
+                        return (
+                            <Card style={{ width: 250, height: 330, marginRight: 10 }} key={recommendation.id}>
+                                <div
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        alignContent: 'space-between',
+                                        alignItems: 'space-between',
+                                        justifyContent: 'space-between',
+                                        justifyItems: 'space-between',
+                                        height: '100%',
+                                    }}
+                                >
+                                    <div className={classes.cardcontainer}>
+                                        <h3>{recommendation.name}</h3>
+                                        <div>
+                                            {recommendation.categories.map((category) => {
+                                                return (
+                                                    <Chip
+                                                        className={classes.chip}
+                                                        color="primary"
+                                                        size="small"
+                                                        label={categoryMappings[category]}
+                                                    />
+                                                );
+                                            })}
+                                        </div>
+                                        <div style={{ fontSize: 14 }} className={classes.description}>
+                                            {recommendation.description.substr(0, 200) + '...'}
+                                        </div>
+                                    </div>
+                                    <Button
+                                        className={classes.button}
+                                        variant="contained"
+                                        color="primary"
+                                        disableElevation
+                                        style={{ width: '100%' }}
+                                    >
+                                        Add to fund
+                                    </Button>
+                                </div>
+                            </Card>
+                        );
+                    })}
                 </div>
             </div>
         );
